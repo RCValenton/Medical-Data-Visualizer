17bb1500397da9beb56dc945fdebe4ffb05ee673
Ryan Valenton <RyanChristopher.Valenton@gmail.com>
2025-05-16 00:12:47 +0000

    Included Instructions

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1 Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
-# 2
+# 2 Add an overweight column to data --> Calculate BMI = weight/height 
 df['overweight'] = None
 
-# 3
+# 3 Normalize 'cholesterol' and 'glucose' so 0 = good value and 1 = bad value
 
 
-# 4
+# 4 Drawing the categorical plot 
 def draw_cat_plot():
-    # 5
+    # 5 Create the DataFrame
     df_cat = None
 
 
-    # 6
+    # 6 Formatting the data
     df_cat = None
     
 
-    # 7
+    # 7 Draw the catplot
 
 
 
-    # 8
+    # 8 Fig for output
     fig = None
 
 
-    # 9
+    # 9 Save the fig
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
+# 10 Drawing Heat map 
 def draw_heat_map():
-    # 11
+    # 11 Clean the data
     df_heat = None
 
-    # 12
+    # 12 Calculate the corr. matrix
     corr = None
 
-    # 13
+    # 13 Create a mask for upper triangle
     mask = None
 
 
 
-    # 14
+    # 14 Matplot fig
     fig, ax = None
 
-    # 15
+    # 15 Draw the heatmap
 
 
 
-    # 16
+    # 16 Save the fig
     fig.savefig('heatmap.png')
     return fig
